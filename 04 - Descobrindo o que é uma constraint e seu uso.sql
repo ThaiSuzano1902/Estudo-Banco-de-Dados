376e6aa99b99b0fe56b210ea40a348d877e873c2
thaisa <thaisasnow2@gmail.com>
2026-07-08 11:27:51 -0300

    docs: Descobrindo o que é uma constraint e seu uso

---
+++ 04 - Descobrindo o que é uma constraint e seu uso.sql
@@ -0,0 +1,39 @@
+-- Isso é uma DML (Data Manipulation Language), manipulação de dados
+EXEC CadastrarCliente
+    @NOME_CLIENTE = 'Fábio Pacce Alves',
+    @CPF = '89103976109',
+    @DATA_NASCIMENTO = '1976-07-12',
+    @RENDA_MENSAL = 2300.00,
+    @PROFISSAO = 'Dublador',
+    @SCORE = 289,
+    @STATUS_CLI = 'Pendente',
+    @SEXO = 'M';
+
+SELECT * FROM Clientes
+
+-- Acrescentando a coluna "AREA".
+-- Caso nenhum valor seja informado, o SQL Server preencherá automaticamente com 'PREENCHER'.
+ALTER TABLE Clientes
+ADD AREA VARCHAR(10) NOT NULL DEFAULT 'PREENCHER';
+
+
+-- Ao utilizar DEFAULT, o SQL Server cria uma CONSTRAINT (restrição).
+-- Constraint = uma regra aplicada pelo SQL Server para garantir a integridade dos dados.
+-- Neste caso, a regra é: "Se AREA não for informada, utilize 'PREENCHER'."
+-- Enquanto essa constraint existir, a coluna não poderá ser excluída diretamente.
+
+-- Para descobrir o nome da constraint criada:
+EXEC sp_help 'Clientes';
+
+-- Removendo a constraint:
+ALTER TABLE Clientes
+DROP CONSTRAINT DF__Clientes__AREA__6FE99F9F;
+
+-- Agora a coluna pode ser removida:
+ALTER TABLE Clientes
+DROP COLUMN AREA;
+
+--Então a regra geral do SQL Server é:
+--"Eu nunca removo automaticamente objetos que dependem de outro objeto. O DBA ou desenvolvedor precisa decidir o que remover."
+-- Por isso o ideal é eu dropar a constraint (a regra que criei), onde eu vejo o nome pelo sp_help, porque assim eu asseguro que a minha coluna não afete outros objetos ou objetos que dependem dele
+
